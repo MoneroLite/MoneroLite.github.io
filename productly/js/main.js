2bb6b2f295fa8775cd9dad977b3707454a2d7c69
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2020-12-09 18:33:10 +0500

    добавлен адаптив

--- productly/js/main.js
+++ productly/js/main.js
@@ -4,7 +4,24 @@ $(function(){
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
-        dots: true
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                
+                     }
+            }
+            ,{
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                
+                     }
+            }]
     });
 
 
